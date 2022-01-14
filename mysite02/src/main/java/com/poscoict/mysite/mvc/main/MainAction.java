aafe79ccccdd73d08d9d576030dcab5364b8e9c5
BIT <BIT@DESKTOP-28SDCPC>
2022-01-14 15:42:39 +0900

    mysite02 first commit

---
+++ mysite02/src/main/java/com/poscoict/mysite/mvc/main/MainAction.java
@@ -0,0 +1,24 @@
+  
+package com.poscoict.mysite.mvc.main;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.poscoict.web.mvc.Action;
+import com.poscoict.web.util.MvcUtil;
+
+public class MainAction implements Action {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/main/index.jsp");
+		rd.forward(request, response);
+		
+		
+		MvcUtil.forward("main/index", request, response);
+	}
+}
---
+++ mysite02/src/main/java/com/poscoict/mysite/mvc/user/UserActionFactory.java
@@ -0,0 +1,14 @@
+package com.poscoict.mysite.mvc.user;
+
+import com.poscoict.web.mvc.Action;
+import com.poscoict.web.mvc.ActionFactory;
+
+public class UserActionFactory extends ActionFactory {
+
+	@Override
+	public Action getAction(String actionName) {
+//		if()
+		return null;
+	}
+
+}
