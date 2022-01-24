89c990c361b483a9425631b1546755f0277d1711
BIT <BIT@DESKTOP-28SDCPC>
2022-01-24 10:48:52 +0900

    mysite03

--- mysite03/src/main/java/com/poscoict/mysite/controller/MainController.java
+++ mysite03/src/main/java/com/poscoict/mysite/controller/MainController.java
@@ -2,14 +2,12 @@ package com.poscoict.mysite.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
 
-	@ResponseBody //임시적
 	@RequestMapping({"", "/main"})
 	public String index() {
-		return "main";
+		return "/WEB-INF/views/main/index.jsp";
 	}
 }
