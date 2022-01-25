0827f07429dd6b3df7b08b3212569130fe2e1e75
BIT <BIT@DESKTOP-28SDCPC>
2022-01-25 15:53:15 +0900

    mysite03 mybatis 적용

--- mysite03/src/main/java/com/poscoict/mysite/repository/UserRepository.java
+++ mysite03/src/main/java/com/poscoict/mysite/repository/UserRepository.java
@@ -1,13 +1,9 @@
 package com.poscoict.mysite.repository;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
-import javax.sql.DataSource;
-
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -16,219 +12,28 @@ import com.poscoict.mysite.vo.UserVo;
 
 @Repository
 public class UserRepository {
-	
 	@Autowired
-	private DataSource dataSource;
+	private SqlSession sqlSession;
 	
-	public boolean update(UserVo userVo) {
-		boolean result = false;
-
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-
-		try {
-			conn = dataSource.getConnection();
-
-			if(userVo.getPassword()==null ||"".equals(userVo.getPassword())) {
-				
-				String sql = "update user set name=?, gender=? where no=? ";
-
-				pstmt = conn.prepareStatement(sql);
-				pstmt.setString(1, userVo.getName());
-				pstmt.setString(2, userVo.getGender());
-				pstmt.setLong(3, userVo.getNo());
-				
-			}else {
-				String sql = "update user set name=?, gender=?, password=? where no=? ";
-
-				pstmt = conn.prepareStatement(sql);
-
-				pstmt.setString(1, userVo.getName());
-				pstmt.setString(2, userVo.getGender());
-				pstmt.setString(3, userVo.getPassword());
-				pstmt.setLong(4, userVo.getNo());
-				
-				int count = pstmt.executeUpdate();
-				result = count == 1;
-
-			}
-
-		} catch (SQLException e) {
-			System.out.println("error:" + e);
-		} finally {
-			try {
-				if (pstmt != null) {
-					pstmt.close();
-				}
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-
-		return result;
+	public boolean update(UserVo vo) {
+		int count = sqlSession.update("user.update", vo);
+		return count == 1;
 	}
 	
 	public boolean insert(UserVo vo) {
-		boolean result = false;
-
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		try {
-			conn = dataSource.getConnection();
-
-			String sql = " insert" + "   into user" + " values(null, ?, ?, ?, ?, now())";
-			pstmt = conn.prepareStatement(sql);
-
-			pstmt.setString(1, vo.getName());
-			pstmt.setString(2, vo.getEmail());
-			pstmt.setString(3, vo.getPassword());
-			pstmt.setString(4, vo.getGender());
-
-			int count = pstmt.executeUpdate();
-			result = count == 1;
-
-		} catch (SQLException e) {
-			System.out.println("error:" + e);
-		} finally {
-			try {
-				if (pstmt != null) {
-					pstmt.close();
-				}
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-
-		return result;
-	}
-
-	public UserVo findByNo(Long userNo) {
-		UserVo result = null;
-
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-
-		try {
-			conn = dataSource.getConnection();
-
-			String sql = "select no, name, email, gender from user where no=?";
-
-			pstmt = conn.prepareStatement(sql);
-
-			pstmt.setLong(1, userNo);
-	
-			rs = pstmt.executeQuery();
-			if (rs.next()) {
-				//update.jsp에서 받아와야할거 가져옴
-				Long no = rs.getLong(1);
-				String name = rs.getString(2);
-				String email=rs.getString(3);
-				String gender=rs.getString(4);
-				
-				result = new UserVo();
-				result.setNo(no);
-				result.setName(name);
-				result.setEmail(email); //사용하려고 뽑음(객체에 넣음)
-				result.setGender(gender);
-
-			}
-
-		} catch (SQLException e) {
-			System.out.println("error:" + e);
-		} finally {
-			try {
-				if (rs != null) {
-					rs.close();
-				}
-				if (pstmt != null) {
-					pstmt.close();
-				}
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		
-
-
-		return result;
-	}
-	public UserVo findByEmailAndPassword(String email, String password) throws UserRepositoryException{ 
-		UserVo result = null;
-
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-
-		try {
-			conn = dataSource.getConnection();
-
-			String sql = "select no, name from user where email=? and password=?";
-
-			pstmt = conn.prepareStatement(sql);
-
-			pstmt.setString(1, email);
-			pstmt.setString(2, password);
-
-			rs = pstmt.executeQuery();
-			if (rs.next()) {
-				Long no = rs.getLong(1);
-				String name = rs.getString(2);
-			
-				
-				result = new UserVo();
-				result.setNo(no);
-				result.setName(name);
-				result.setEmail(email); //사용하려고 뽑음(객체에 넣음)
-
-			}
-
-		} catch (SQLException e) {
-			throw new UserRepositoryException(e.toString());
-		} finally {
-			try {
-				if(rs != null) {
-					rs.close();
-				}
-				if(pstmt != null) {
-					pstmt.close();
-				}
-				if(conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}		
-		
-		return result;
+		int count = sqlSession.insert("user.insert", vo);
+		return count == 1;
 	}	
 
+	public UserVo findByNo(Long userNo) {
+		return sqlSession.selectOne("user.findByNo", userNo);
+	}
 
-
-//	private Connection getConnection() throws SQLException {
-//		Connection conn = null;
-//		try {
-//			Class.forName("com.mysql.cj.jdbc.Driver");
-//			String url = "jdbc:mysql://localhost:3306/webdb?characterEncoding=UTF-8&serverTimezone=UTC";
-//			conn = DriverManager.getConnection(url, "webdb", "webdb");
-//		} catch (ClassNotFoundException e) {
-//			System.out.println("드라이버 로딩 실패:" + e);
-//		}
-//
-//		return conn;
-//	}
-
-
-
+	public UserVo findByEmailAndPassword(String email, String password) throws UserRepositoryException {
+		Map<String, String> map = new HashMap<>();
+		map.put("e", email);
+		map.put("p", password);
+		
+		return sqlSession.selectOne("user.findByEmailAndPassword", map);
+	}	
 }
